54f4473b175394b6885d1c546669fe6cdbdae24b
Carlosreyes2002 <105017046+Carlosreyes2002@users.noreply.github.com>
2024-03-09 12:19:19 -0600

    Parcial I Progra III

---
+++ Algoritmo.java
@@ -0,0 +1,50 @@
+public class Algoritmo {
+    
+    public static void main(String[] args) {
+        int[] array = {5, 9, 3, 1, 6, 2, 7};
+        System.out.println("Desordenado:");
+        printArray(array);
+        
+        quickSort(array, 0, array.length - 1);
+        
+        System.out.println("Ordenado:");
+        printArray(array);
+    }
+    
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+    
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        
+        swap(array, i + 1, high);
+        
+        return i + 1;
+    }
+    
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
